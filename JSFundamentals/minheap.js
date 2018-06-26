aa06f6895b3eadb11ad4997bda7e402f67d1c912
isabelchacon <isabel@cogwheelclinic.com>
2018-06-25 20:13:29 -0700

    Remove from Heap

--- JSFundamentals/minheap.js
+++ JSFundamentals/minheap.js
@@ -20,8 +20,41 @@ function insertIntoHeap(heap, val){
     findplace(heap, index);
     console.log(heap);
     console.log(heap.length);
-    // your code here
 }
-// sample outputs:
-insertIntoHeap([], 13);  // alters the empty array to be [undefined, 13]
-insertIntoHeap([undefined, 3, 8, 10, 11, 9, 20, 14], 7); // alters the array to [undefined, 3, 7, 10, 8, 9, 20, 14, 11]
+
+
+function removeFromMinHeap(heap){
+    // your code here
+
+    var i = 1;
+    var temp = heap[heap.length - 1];
+    heap[heap.length - 1] = heap[i];
+    heap[i]= temp;
+
+    var value = heap.pop();
+
+    var low = heap[i * 2] < heap[i * 2 + 1] ? i * 2 : i * 2 + 1;
+    
+    while (heap[i]>heap[low]){
+
+        let temp = heap[low];
+        heap[low]=heap[i];
+        heap[i]= temp;
+        i = low;
+
+        low = heap[i * 2] < heap[i * 2 + 1] ? i * 2 : i * 2 + 1;
+
+    }
+
+    console.log(heap);
+    return value;
+
+}
+
+
+
+insertIntoHeap([], 13);  
+insertIntoHeap([undefined, 3, 8, 10, 11, 9, 20, 14], 7); 
+console.log(removeFromMinHeap([undefined, 3, 12, 8, 17, 13, 15, 10]));
+console.log(removeFromMinHeap([undefined, 8]));
+
